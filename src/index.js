9822e7a96b51ce64e8bdbeb6507c53570aeccffa
Matheus Braun <matheeusbl@gmail.com>
2020-02-22 22:32:10 -0300

    Setting up not found and error handler middlewares

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
+
+const { notFound, errorHandler } = require('./middlewares');
 
 const app = express();
 
 app.use(morgan('common'));
 app.use(helmet());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Listening at http://localhost:${port}`);
 });
