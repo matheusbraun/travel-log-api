5c9975889e024b19ba7b5f27ed7d5f6c2d26611d
Matheus Braun <matheeusbl@gmail.com>
2020-02-22 23:19:24 -0300

    Creating LogEntry Schema and setting up .env variables

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const { notFound, errorHandler } = require('./middlewares');
 
 const app = express();
 
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
   }),
 );
 
---
+++ src/model/LogEntry.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const requiredNumber = {
+  type: Number,
+  required: true,
+};
+
+const logEntrySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    comments: String,
+    rating: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    image: String,
+    latitude: {
+      ...requiredNumber,
+      min: -90,
+      max: 90,
+    },
+    longitude: {
+      ...requiredNumber,
+      min: -180,
+      max: 180,
+    },
+    visitDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const LogEntry = mongoose.model('LogEntry', logEntrySchema);
+module.exports = LogEntry;
