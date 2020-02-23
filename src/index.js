1e6800ad948a83a892657b3de40ac37803cec451
Matheus Braun <matheeusbl@gmail.com>
2020-02-22 21:47:21 -0300

    Setting up morgan and helment
    Morgan added to show requests logs and helmet to increase the app security.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(morgan('common'));
+app.use(helmet());
 
 const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
-  console.log('Listening at http://localhost:3333');
+  console.log(`Listening at http://localhost:${port}`);
 });
